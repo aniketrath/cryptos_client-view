66ca3db03c1d216481d735647bfa8bcc6e84d9ee
Aniket Rath <aniketrath1121@gmail.com>
2024-12-24 07:03:05 +0530

    added Nginx to manage connection

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,17 @@
-# Use the official Node.js image as the base image
-FROM node:18 AS development
+# Build Stage
+FROM node:18 AS build
 WORKDIR /app
 COPY package.json package-lock.json ./
 RUN npm install --force
 COPY . .
-EXPOSE 3000
-CMD ["npm", "start"]
+RUN npm run build
+
+# Nginx Stage
+FROM nginx:alpine
+# Copy custom Nginx config file
+COPY nginx.conf /etc/nginx/nginx.conf
+# Copy React build output to Nginx's html directory
+COPY --from=build /app/build /usr/share/nginx/html
+EXPOSE 80
+# Run nginx in the foreground
+CMD ["nginx", "-g", "daemon off;"]
--- src/pages/APIs.js
+++ src/pages/APIs.js
@@ -1,119 +1,85 @@
-const baseURL = process.env.REACT_APP_API_URL; // Get base URL from environment variable
+import axios from "axios";
 
-// Function to fetch suggestions
+const axiosInstance = axios.create({
+  baseURL: process.env.REACT_APP_API_URL, // Get base URL from environment variable
+});
+
 export const fetchSuggestions = async () => {
   try {
-    const response = await fetch(`${baseURL}/app/suggestions`);
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    const data = await response.json(); // Parse the response as JSON
-    return data;
+    const response = await axiosInstance.get("app/suggestions");
+    return response.data;
   } catch (error) {
     console.error("[API ERROR]", error.message || error);
     throw error;
   }
 };
 
-// Function to fetch gainers
 export const fetchGainers = async () => {
   try {
-    const response = await fetch(`${baseURL}/app/gainers`);
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    const data = await response.json(); // Parse the response as JSON
-    return data;
+    const response = await axiosInstance.get("app/gainers");
+    return response.data;
   } catch (error) {
     console.error("[API ERROR]", error.message || error);
     throw error;
   }
 };
 
-// Function to fetch losers
 export const fetchLossers = async () => {
   try {
-    const response = await fetch(`${baseURL}/app/losers`);
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    const data = await response.json(); // Parse the response as JSON
-    return data;
+    const response = await axiosInstance.get("app/losers");
+    return response.data;
   } catch (error) {
     console.error("[API ERROR]", error.message || error);
     throw error;
   }
 };
 
-// Function to fetch all coins
 export const fetchAllCoins = async () => {
   try {
-    const response = await fetch(`${baseURL}/app/get/coin?id=all`);
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    const data = await response.json(); // Parse the response as JSON
-    return data;
+    const response = await axiosInstance.get("app/get/coin?id=all");
+    return response.data;
   } catch (error) {
     console.error("[API ERROR]", error.message || error);
     throw error;
   }
 };
 
-// Function to fetch BTC details
 export const fetchBTCDetails = async () => {
   try {
-    const response = await fetch(`${baseURL}/app/get/coin?id=btc-bitcoin`);
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    const data = await response.json(); // Parse the response as JSON
-    return data;
+    const response = await axiosInstance.get("/app/get/coin", {
+      params: { id: "btc-bitcoin" },
+    });
+    return response.data;
   } catch (error) {
     console.error("[API ERROR]", error.message || error);
     throw error;
   }
 };
-
-// Function to fetch ETH details
 export const fetchETHDetails = async () => {
   try {
-    const response = await fetch(`${baseURL}/app/get/coin?id=eth-ethereum`);
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    const data = await response.json(); // Parse the response as JSON
-    return data;
+    const response = await axiosInstance.get("/app/get/coin", {
+      params: { id: "eth-ethereum" },
+    });
+    return response.data;
   } catch (error) {
     console.error("[API ERROR]", error.message || error);
-    throw error;
+    throw error; // Re-throw error for handling in the component
   }
 };
-
-// Function to fetch global details
 export const fetchGlobalDetails = async () => {
   try {
-    const response = await fetch(`${baseURL}/app/global/status`);
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    const data = await response.json(); // Parse the response as JSON
-    return data;
+    const response = await axiosInstance.get(`/app/global/status`);
+    return response.data;
   } catch (error) {
     console.error("[API ERROR]", error.message || error);
     throw error;
   }
 };
 
-// Function to fetch stats
 export const fetchStats = async (id) => {
   try {
-    const response = await fetch(`${baseURL}/app/get/coin?id=${id}`);
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    const data = await response.json(); // Parse the response as JSON
-    return data;
+    const response = await axiosInstance.get(`/app/get/coin?id=${id}`);
+    return response.data;
   } catch (error) {
     console.error("[API ERROR]", error.message || error);
     throw error;
